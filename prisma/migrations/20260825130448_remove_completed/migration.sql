d70707cb7fee4e5e859034ff0e5ef61737f85c89
kabirsib349 <kabiribrahim349@gmail.com>
2026-08-26 07:01:11 +0200

    db: migration vers PostgreSQL (Supabase) et retrait de la colonne completed

---
+++ prisma/migrations/20260825130448_remove_completed/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `completed` on the `Note` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Note" DROP COLUMN "completed";
